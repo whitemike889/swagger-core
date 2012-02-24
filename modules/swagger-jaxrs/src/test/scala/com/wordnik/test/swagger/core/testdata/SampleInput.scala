871f7a7d7dc9874cc679d5a075be6468c07ab685
rpidikiti <ramesh@wordnik.com>
2012-02-23 17:17:24 -0800

    Updated test cases to handle annotation incostructor arguments

--- modules/swagger-jaxrs/src/test/scala/com/wordnik/test/swagger/core/testdata/SampleInput.scala
+++ modules/swagger-jaxrs/src/test/scala/com/wordnik/test/swagger/core/testdata/SampleInput.scala
@@ -4,6 +4,7 @@ import javax.xml.bind.annotation.XmlRootElement._
 import reflect.BeanProperty
 import javax.xml.bind.annotation.XmlElement._
 import javax.xml.bind.annotation.{XmlAccessType, XmlAccessorType, XmlElement, XmlRootElement}
+import annotation.target.field
 
 /**
  * User: ramesh
@@ -11,11 +12,12 @@ import javax.xml.bind.annotation.{XmlAccessType, XmlAccessorType, XmlElement, Xm
  * Time: 12:05 PM
  */
 @XmlRootElement(name = "sampleInput")
+@XmlAccessorType(XmlAccessType.NONE)
 case class SampleInput (
-    @XmlElement(name = "name") @BeanProperty
+    @(XmlElement @field)(name = "name") @BeanProperty
     var name: String,
 
-    @XmlElement(name = "value") @BeanProperty
+    @(XmlElement @field)(name = "value") @BeanProperty
     var value: String
 ){
   def this() = this(null,null)
