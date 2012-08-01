4e4489e00bd098c13c774d3c221b19f5b099d148
Tony Tam <fehguy@gmail.com>
2012-07-31 21:42:32 -0700

    updated to jackson 2.0, play 2.0.2

--- samples/scala-play2/project/Build.scala
+++ samples/scala-play2/project/Build.scala
@@ -12,10 +12,11 @@ object ApplicationBuild extends Build {
     "org.codehaus.jackson" % "jackson-xc" % "1.9.5",
     "org.codehaus.jackson" % "jackson-mapper-asl" % "1.9.5",
     "org.codehaus.jackson" % "jackson-core-asl" % "1.9.5",
-    "com.wordnik" %% "swagger-play2-utils" % "1.0.2")
+    "com.wordnik" %% "swagger-play2-utils" % "1.1.0-SNAPSHOT")
 
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
     resolvers := Seq(
+      "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository",
       Resolver.url("swagger-core-github-repo", url("http://wordnik.github.com/repository"))(Resolver.ivyStylePatterns),
       "java-net" at "http://download.java.net/maven/2",
       "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"))
