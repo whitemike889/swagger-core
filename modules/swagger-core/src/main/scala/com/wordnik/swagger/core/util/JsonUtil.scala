10c269181459e97cccfef1a7eb1675467d8356d5
Tony Tam <fehguy@gmail.com>
2012-08-10 01:42:32 -0700

    unset timestamps for dates

--- modules/swagger-core/src/main/scala/com/wordnik/swagger/core/util/JsonUtil.scala
+++ modules/swagger-core/src/main/scala/com/wordnik/swagger/core/util/JsonUtil.scala
@@ -29,6 +29,7 @@ object JsonUtil {
 //    mapper.registerModule(new DefaultScalaModule())
     mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
     mapper.setSerializationInclusion(JsonInclude.Include.NON_DEFAULT)
+    mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
     mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
     mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY)
     mapper
