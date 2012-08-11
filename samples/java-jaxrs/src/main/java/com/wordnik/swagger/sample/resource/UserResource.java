9b6eaa2e8bea0f5aeb2bee28e178d24872728a9b
Tony Tam <fehguy@gmail.com>
2012-08-11 11:56:25 -0700

    updated response mappings, formatting

--- samples/java-jaxrs/src/main/java/com/wordnik/swagger/sample/resource/UserResource.java
+++ samples/java-jaxrs/src/main/java/com/wordnik/swagger/sample/resource/UserResource.java
@@ -17,12 +17,12 @@
 package com.wordnik.swagger.sample.resource;
 
 import com.wordnik.swagger.annotations.*;
-import com.wordnik.swagger.jaxrs.*;
-
 import com.wordnik.swagger.sample.data.UserData;
 import com.wordnik.swagger.sample.model.User;
 import com.wordnik.swagger.sample.exception.ApiException;
 import com.wordnik.swagger.sample.exception.NotFoundException;
+import com.wordnik.swagger.jaxrs.JavaHelp;
+
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.*;
