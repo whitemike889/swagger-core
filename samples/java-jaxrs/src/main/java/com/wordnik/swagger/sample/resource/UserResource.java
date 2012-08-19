5212d14b2244f19e9a52545765da21ef4b65f15c
Tony Tam <fehguy@gmail.com>
2012-08-18 21:39:57 -0700

    fixed return type, test validation

--- samples/java-jaxrs/src/main/java/com/wordnik/swagger/sample/resource/UserResource.java
+++ samples/java-jaxrs/src/main/java/com/wordnik/swagger/sample/resource/UserResource.java
@@ -102,7 +102,7 @@ public class UserResource extends JavaHelp {
 
 	@GET
 	@Path("/login")
-	@ApiOperation(value = "Logs user into the system", responseClass = "String")
+	@ApiOperation(value = "Logs user into the system", responseClass = "string")
 	@ApiErrors(value = { @ApiError(code = 400, reason = "Invalid username/password supplied") })
 	public Response loginUser(
 			@ApiParam(value = "The user name for login", required = true) @QueryParam("username") String username,
